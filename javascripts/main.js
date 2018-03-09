38dd7a57990f7278fa86e23e34be65f3c152b62e
frbl <frank.blaauw@gmail.com>
2018-03-09 16:15:00 +0100

    Added the available flag

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var repo = 'compsy/compsy.github.io';
   var content = $('#content');
-  $.getJSON('https://api.github.com/repos/' + repo + '/issues?labels=Project', function(data) {
+  $.getJSON('https://api.github.com/repos/' + repo + '/issues?labels=Project,Available', function(data) {
     var entry;
     var resp = '';
     for (var i = 0, len = data.length; i < len; i++) {
